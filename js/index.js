ae5242a1ff8260aa0499af01db01377729eab3cc
System Administrator <root@PhilMacBook.local>
2020-01-18 17:00:16 +0000

    Updating revival-direct-coaches

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ AOS.init({
   duration: 1200
 });
 
-
+var itemsToClear = document.getElementById('labeldateout')
+itemsToClear.addEventListener('click', function () {
+  alert('clicked')
+})
+console.log(itemsToClear)
